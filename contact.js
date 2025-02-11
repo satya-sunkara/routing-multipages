822bcc962761fcfcd9809a5e43e96f34f40a54b6
satya-sunkara <satya521002@gmail.com>
2025-02-11 23:37:43 +0530

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+const Contact = () => {
+    return (
+        <>
+            
+            <h2 className="name">Contact page</h2>
+            
+            <Link to="/">Home</Link>
+            
+
+        </>
+       
+    )
+
+
+}
+
+export default Contact
