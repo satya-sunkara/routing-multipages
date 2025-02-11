7c16864f71a73ee46c1181f4dc5e12dd5d71b58e
satya-sunkara <satya521002@gmail.com>
2025-02-11 23:37:01 +0530

    Create about.js

---
+++ about.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+const About = () => {
+    return (
+        <>
+            
+            <h2 className="name">About page</h2>
+            
+            <Link to="/">Home</Link>
+            
+
+        </>
+       
+    )
+
+
+}
+
+export default About
