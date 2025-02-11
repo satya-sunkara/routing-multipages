57a1f5e2307e58d8652bcd7cfab37ce7fbe59cd2
satya-sunkara <satya521002@gmail.com>
2025-02-11 23:33:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js'
+import "./practise.css"
+
+
+ReactDOM.render(<App/>,document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
