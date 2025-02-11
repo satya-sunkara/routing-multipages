5715743fa8fc5abd72fb899cec4e972eeb0e8146
satya-sunkara <satya521002@gmail.com>
2025-02-11 23:38:35 +0530

    Create product.js

---
+++ product.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+const Product = () => {
+    return (
+        <>
+            
+            <h2 className="name">Product page</h2>
+            
+            <Link to="/">Home</Link>
+            
+
+        </>
+       
+    )
+
+
+}
+
+export default Product
